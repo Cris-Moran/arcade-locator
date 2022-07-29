950c6222374e43c8334cdf625ead74a510187292
Cris-Moran <68084820+Cris-Moran@users.noreply.github.com>
2022-07-28 17:19:14 -0700

    fixed bugs with registering

--- app/src/main/java/com/example/arcadefinder/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/arcadefinder/Activities/RegisterActivity.java
@@ -1,10 +1,18 @@
 package com.example.arcadefinder.Activities;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +24,8 @@ import com.example.arcadefinder.R;
 import com.example.arcadefinder.ViewModels.RegisterViewModel;
 import com.parse.ParseUser;
 
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
 
     public static final String TAG = "Register Activity";
@@ -25,6 +35,7 @@ public class RegisterActivity extends AppCompatActivity {
     Button btnRegister;
     RegisterViewModel registerViewModel;
     boolean firstObservation = true;
+    boolean locationPermission;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +53,7 @@ public class RegisterActivity extends AppCompatActivity {
                 ParseUser user = registerModel.getUser();
                 if (user != null) {
                     Toast.makeText(RegisterActivity.this, "Registered!", Toast.LENGTH_SHORT).show();
-                    goMainActivity();
+                    goLoginActivity();
                 } else if (firstObservation) {
                     // When activity is created user will be null, don't want to say failed to register
                     firstObservation = false;
@@ -63,9 +74,10 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
-    private void goMainActivity() {
-        Intent i = new Intent(this, MainActivity.class);
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
         startActivity(i);
+        finishAffinity();
         finish();
     }
 }
